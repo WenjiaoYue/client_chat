fa1229b81a0ad5a3d88a45fa060b06a2acfa90e1
Yue, Wenjiao <wenjiao.yue@intel.com>
2023-09-21 10:24:18 +0800

    modify ui

--- front_end/src/routes/+page.svelte
+++ front_end/src/routes/+page.svelte
@@ -271,7 +271,7 @@
 			{/if}
 
 			<div
-				class="relative flex w-full flex-col items-center justify-between bg-white p-2  shadow-inner fixed inset-x-0 bottom-0 z-40"
+				class="relative flex w-full flex-col items-center justify-between bg-white p-2  shadow-inner fixed z-40 bottom-16 left-4"
 			>
 				{#if uploadProgress}
 					<Progressbar
