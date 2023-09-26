dee4ef5e07b1d6d5bd358a65a497892f1d01ee24
Yue, Wenjiao <wenjiao.yue@intel.com>
2023-09-26 15:50:59 +0800

    hide chrome side bar

--- front_end/src/routes/+layout.svelte
+++ front_end/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import "tailwindcss/tailwind.css";
-	import "../app.postcss"
+	import "../app.postcss";
 	import Notifications from "svelte-notifications";
 	import Layout from "$lib/modules/frame/Layout.svelte";
 	import { onMount } from "svelte";
@@ -12,15 +12,19 @@
 		window.onresize = () => {
 			window.deviceType = window.innerWidth > 640 ? "pc" : "mobile";
 		};
+		window.addEventListener("load", function () {
+			setTimeout(function () {
+				// This hides the address bar:
+				window.scrollTo(0, 1);
+			}, 0);
+		});
 	});
-
-
 </script>
 
 <Notifications>
 	<Layout>
-		<div class="h-full flex flex-col">
-			<Marquee/>
+		<div class="flex h-full flex-col">
+			<Marquee />
 			<Timer />
 			<div class="h-0 grow">
 				<slot />
