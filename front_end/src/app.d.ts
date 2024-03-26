811306947b51b0045016c40f1d0e01bf7a7b451a
Yue, Wenjiao <wenjiao.yue@intel.com>
2024-03-26 15:49:56 +0800

    web code gen

--- front_end/src/app.d.ts
+++ front_end/src/app.d.ts
@@ -1,5 +1,13 @@
-// See: https://kit.svelte.dev/docs/types#app
-// import { Result} from "neverthrow";
-interface Window {
-	deviceType: string;
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface PageState {}
+		// interface Platform {}
+	}
 }
+
+export {};
